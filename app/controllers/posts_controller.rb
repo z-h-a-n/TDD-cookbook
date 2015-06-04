de11456745b53d03ae1f61cd9b80cd2971dcf888
zhan wang <zhanzw@gmail.com>
2015-06-04 23:18:40 +0100

    finished testing post feature

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,8 +1,27 @@
 class PostsController < ApplicationController
 
 	def index	
+		@posts = Post.all
 	end
 
+	def new
+		@post = Post.new
+	end
+
+	def create
+		post = Post.new params[:post].permit(:title, :content)
+
+		if post.save
+			redirect_to posts_path
+		else
+			render 'new'
+		end
+	end
+
+	def show
+		@post = Post.find(params[:id])
+
+	end
 
 
 end
--- spec/features/post_management_spec.rb
+++ spec/features/post_management_spec.rb
@@ -4,7 +4,13 @@ describe 'adding a post' do
 
   it "a user can add a post to the site" do
     visit '/'
-  
+    click_link 'Add a post'
+    fill_in 'Title', with: 'mushroom risotto'
+    fill_in 'Content', with: 'cooking time: 20mins'
+    click_button 'Submit'
+
+    expect(page).to have_content 'mushroom risotto'
+
   end
 
 end
